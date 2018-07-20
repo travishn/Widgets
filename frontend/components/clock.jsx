3c1a985adc47d403d6d4029172a97365fa25fc3d
Travis <travishn@uci.edu>
2018-07-19 17:32:54 -0700

    Create months hash and reconfigured logic for tick function

--- frontend/components/clock.jsx
+++ frontend/components/clock.jsx
@@ -13,6 +13,21 @@ class Clock extends React.Component {
       6: 'Saturday',
       7: 'Sunday'
     };
+
+    this.months= {
+      1: { name: 'Jan', max_day: 31 },
+      2: { name: 'Feb', max_day: 28 },
+      3: { name: 'Mar', max_day: 31 },
+      4: { name: 'Apr', max_day: 30 },
+      5: { name: 'May', max_day: 31 },
+      6: { name: 'Jun', max_day: 30 },
+      7: { name: 'Jul', max_day: 31 },
+      8: { name: 'Aug', max_day: 31 },
+      9: { name: 'Sep', max_day: 30 },
+      10: { name: 'Oct', max_day: 31 },
+      11: { name: 'Nov', max_day: 30 },
+      12: { name: 'Dec', max_day: 31 }
+    };
     
     this.state = {
       hours: this.time.getHours(),
@@ -43,17 +58,34 @@ class Clock extends React.Component {
       this.setState({hours: this.state.hours += 1});
     }
 
-    if (this.state.hours === 0) {
+    if (this.state.hours === 24) {
       this.setState({hours: 0});
+      this.setState({day: this.state.day += 1});
     }
 
+    if (this.state.day === this.months[this.state.month].max_day + 1) {
+      this.setState({day: 1});
+      this.setState({month: this.state.month += 1 });
+    }
+
+    if (this.state.month === 13) {
+      this.setState({month: 1});
+      this.setState({year: this.state.year += 1});
+    }
   }
 
   render() {
     return (
       <div>
-        <h1>Ze Clock</h1>
-        <p>{this.state.hours}:{this.state.minutes}:{this.state.seconds}</p>
+        <h2>Clock</h2>
+        <div>
+          <h5>Time: </h5>
+          <p>{this.state.hours}:{this.state.minutes}:{this.state.seconds}</p>
+        </div>
+        <div>
+          <h5>Date:</h5>
+          <p>{this.days[this.state.day]} {this.months[this.state.month].name}  {this.state.day}, {this.state.year}</p>
+        </div>
       </div>
     );
   }
