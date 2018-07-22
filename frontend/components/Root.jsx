b349ae06c30e838776f23a1c24d06d9ef022ea81
Travis <travishn@uci.edu>
2018-07-21 21:42:37 -0700

    Create weather widget skeleton

--- frontend/components/Root.jsx
+++ frontend/components/Root.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Clock from './clock';
 import Tab from './tab';
 import Autocomplete from './autocomplete';
+import Weather from './weather';
 
 const Root = ({store}) => {
   return (
---
+++ frontend/components/weather.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { APIKey } from '../../CLIENT_SECRET';
+
+class Weather extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <section>
+      </section>
+    );
+  }
+}
+
