a988dc7537cfe3c03d851662f6ed2dfadb665a40
Travis <travishn@uci.edu>
2018-07-21 22:10:32 -0700

    Add API request on component did mount

--- frontend/components/weather.jsx
+++ frontend/components/weather.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import { APIKey } from '../../CLIENT_SECRET';
+import axios from 'axios';
 
 class Weather extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      location: '',
+      temperature: ''
+    };
+  }
+
+  // Would usually hit a frontend route which will hit a backend route which will make the API request
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      let url = "http://api.openweathermap.org/data/2.5/weather?";
+      url += APIKey;
+      url += `&lon=${position.coords.longitude}`;
+      url += `&lat=${position.coords.latitude}`;
+
+      axios.get(url).then( res => {
+        this.setState({temperature: res.main.temp});
+        this.setState({location: res.name});
+      });
+    });
   }
 
   render() {
