f5afcd4656a7abef706e03c9617fb47882e223af
Travis <travishn@uci.edu>
2018-07-22 23:50:36 -0700

    Fix data storage from api reqest

--- frontend/components/weather.jsx
+++ frontend/components/weather.jsx
@@ -13,16 +13,19 @@ class Weather extends React.Component {
 
   // Would usually hit a frontend route which will hit a backend route which will make the API request
   componentDidMount() {
+    let newLocation = '';
+    let newTemp = '';
+    
     navigator.geolocation.getCurrentPosition(function (position) {
       let url = "http://api.openweathermap.org/data/2.5/weather?";
       url += 'APPID=' + APIKey.APIKey;
       url += `&lon=${position.coords.longitude}`;
       url += `&lat=${position.coords.latitude}`;
-      debugger;
 
       axios.get(url).then( res => {
-        this.setState({temperature: res.main.temp});
-        this.setState({location: res.name});
+        newLocation = res.data.name;
+        newTemp = res.data.main.temp;
+        debugger;
       });
     });
   }
